a43052ecf39102cee30fad191a6a977d5ec6d67b
VANN VIREAKROTH <vannvireakroth57@gmail.com>
2025-08-31 23:42:50 +0700

    Update routing setup in main.jsx

--- Pages/Header_File/Header_file.jsx
+++ Pages/Header_File/Header_file.jsx
@@ -3,13 +3,21 @@ import { NavLink, Outlet } from 'react-router'
 
 function Header_file() {
   return (
-    <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/fit_center">Fit Centers</NavLink>
-      <NavLink to="/fit_parther">Fit Parthers</NavLink>
-      <NavLink to="/blogs">Blogs</NavLink>
-      <NavLink to="/appx">App</NavLink>
-      <NavLink to="/help">Help</NavLink>
+    <div>  
+      <div className=''>
+        <NavLink to="/"
+        >Home</NavLink>
+        <NavLink to="/fit_center" className=""
+        >Fit Centers</NavLink>
+        <NavLink to="/fit_parther" className=""
+        >Fit Parthers</NavLink>
+        <NavLink to="/blogs" className=""
+        >Blogs</NavLink>
+        <NavLink to="/appx" className=""
+        >App</NavLink>
+        <NavLink to="/help" className=""
+        >Help</NavLink>
+      </div>
       <Outlet/>
     </div>
   )
